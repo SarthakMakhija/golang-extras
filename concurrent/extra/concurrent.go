37bafecf903aac5fb95efd5e58ea894061982e5c
sarthak.makhija <sarthak.makhija@gmail.com>
2021-08-20 22:28:22 +0530

    Sarthak | Implements Map under concurrent

--- concurrent/extra/concurrent.go
+++ concurrent/extra/concurrent.go
@@ -15,3 +15,19 @@ func Repeat(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
 	}()
 	return outputChannel
 }
+
+func Map(done <-chan interface{}, incoming <-chan interface{}, mapFunc func(interface{}) interface{}) <-chan interface{} {
+
+	outputChannel := make(chan interface{})
+	go func() {
+		defer close(outputChannel)
+		for value := range incoming {
+			select {
+			case <-done:
+				return
+			case outputChannel <- mapFunc(value):
+			}
+		}
+	}()
+	return outputChannel
+}
--- concurrent/extra/concurrent_test.go
+++ concurrent/extra/concurrent_test.go
@@ -21,6 +21,33 @@ func TestRepeat(t *testing.T) {
 
 	expected := []interface{}{1, 1, 1}
 	if !reflect.DeepEqual(elements, expected) {
-		t.Fatalf("Expected %v from repeat, received %v", expected, elements)
+		t.Fatalf("Expected %v from Repeat, received %v", expected, elements)
+	}
+}
+
+func TestMap(t *testing.T) {
+	done := make(chan interface{})
+	defer close(done)
+
+	incomingChannel := make(chan interface{})
+	go func() {
+		defer close(incomingChannel)
+		incomingChannel <- 1
+		incomingChannel <- 2
+		incomingChannel <- 3
+	}()
+
+	outputChannel := extra.Map(done, incomingChannel, func(value interface{}) interface{} {
+		return (value.(int)) * 2
+	})
+
+	var elements []interface{}
+	for mapped := range outputChannel {
+		elements = append(elements, mapped)
+	}
+
+	expected := []interface{}{2, 4, 6}
+	if !reflect.DeepEqual(elements, expected) {
+		t.Fatalf("Expected %v from Map, received %v", expected, elements)
 	}
 }
